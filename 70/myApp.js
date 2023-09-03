35b28ebba44a7e26d53d8a93d5ccafac9b0ade95
NechamiSzimonowitz <Nechami129@gmail.com>
2023-09-03 11:13:46 -0400

    hmwk 70

---
+++ 70/myApp.js
@@ -0,0 +1,31 @@
+window.myApp = (function (module) {
+    module.utils = (function () {
+        'use strict';
+
+        const months = ['January', 'February', 'March', 'April'];
+
+        function foo() {
+            console.log('Foo was called');
+        }
+
+        function getMonth(index) {
+            foo();
+            return months[index - 1];
+        }
+
+        function getMonthIndex(month) {
+            for (let i = 0; i < months.length; i++) {
+                if (months[i] === month) {
+                    return i + 1;
+                }
+            }
+        }
+
+        return {
+
+            getMonth,
+            getMonthIndex
+        };
+    })
+    return module;
+}(window.myApp || {}));
