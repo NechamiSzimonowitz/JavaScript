9f91746d85002ce8be25ddf6a8fd2be1442d6352
NechamiSzimonowitz <Nechami129@gmail.com>
2023-10-29 15:57:35 -0400

    hmwk 80

---
+++ 80/youtube.js
@@ -0,0 +1,13 @@
+/*global $*/
+(async function () {
+    const response = await fetch('songs.json');
+    if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const songs = await response.json();
+    songs.forEach(song => {
+        $('.musicOptions').append(` <a href=${song.link}><img src=${song.image} width="50" height="60" />Song Title: ${song.title}</a> </br>`)
+
+    });
+})();
+
