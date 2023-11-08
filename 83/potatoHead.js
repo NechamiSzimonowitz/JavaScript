5fb5353dc4bb1759a27e9990fbac11201fad86d6
NechamiSzimonowitz <Nechami129@gmail.com>
2023-11-07 22:30:18 -0500

    hmwk 83

---
+++ 83/potatoHead.js
@@ -0,0 +1,50 @@
+/*global $*/
+(function () {
+    let dragging;
+    let offset;
+    let draggingElement;
+    const dragObject = $('.draggable');
+    const locations = [];
+
+    dragObject.on('mousedown', e => {
+        if ($(e.target).hasClass('draggable')) {
+            console.log($(e.target))
+            draggingElement = $((e.target));
+            offset = {
+                y: e.pageY - draggingElement.offset().top,
+                x: e.pageX - draggingElement.offset().left
+            };
+            dragging = true;
+        }
+    })
+
+    $(document).on('mousemove', mouseMoveHandler);
+
+    $(document).on('mouseup', e => {
+        if (draggingElement) {
+            const top = e.pageY - offset.y;
+            const left = e.pageX - offset.x;
+            locations.push({ top, left });
+            draggingElement = null;
+        }
+        dragging = false;
+    })
+
+    function mouseMoveHandler(e) {
+        e.preventDefault();
+        if (dragging) {
+            const top = (e.pageY - offset.y);
+            const left = (e.pageX - offset.x);
+            draggingElement.css({ 'top': top + 'px', 'left': left + 'px' });
+        }
+    }
+    $('#save').on('click', () => {
+        const docName = $('#saveName').val();
+        $('#saveName').val('');
+        localStorage.setItem(docName, JSON.stringify(locations));
+    })
+
+})();
+
+
+
