08088c1579bea5039db7b7ed6e3538dbf473e6c3
NechamiSzimonowitz <Nechami129@gmail.com>
2024-02-13 17:04:03 -0500

    hmwk108

---
+++ 108/Contacts-api/routes/contacts-api.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+router.route('/')
+    .get(async (req, res, next) => {
+        try {
+            const [results] = await global.connection.query(
+                'SELECT * FROM contacts'
+            );
+            res.send(results);
+        } catch (err) {
+            return next(err);
+        }
+    })
+    .post(async (req, res, next) => {
+        const { first, last, email, phone } = req.body;
+        try {
+            const [results] = await global.connection.execute(
+                'INSERT INTO contacts(first, last, email, phone) VALUES(?,?,?,?)', [first, last, email, phone]
+            );
+
+            console.log(results);
+            req.body.id = results.insertId;
+
+            res.status(201)
+                .location(`/contacts-api/${req.body.id}`)
+                .send(req.body);
+        } catch (err) {
+            return next(err);
+        }
+    });
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.end(err.message || 'Internal server error...');
+});
+
+router.get('/:id', async (req, res) => {
+    const contactId = req.params.id;
+
+    try {
+        const [contacts] = await global.connection.execute(
+            'SELECT * FROM contacts WHERE id = ?', [contactId]
+        );
+
+        if (!contacts) {
+            return res.status(404).json({ error: 'contact not found' });
+        }
+
+        res.json(contacts);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'internal server errror' });
+    }
+})
+
+module.exports = router;
